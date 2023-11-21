3d107f82927dbf35953da081b431707fcf893640
caelebk <khocaeleb@gmail.com>
2023-11-20 22:15:55 -0800

    Update deployment of commands to typescript

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,54 +1,26 @@
 import { REST, Routes } from "discord.js";
 import { CLIENT_ID, DEV_SERVER_ID, BOT_TOKEN } from "./config.json";
-import fs from "node:fs";
-import path from "node:path";
+import { Command, commandList } from "./commands/command";
 
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
-// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(BOT_TOKEN);
 
-// and deploy your commands!
 (async () => {
   try {
     console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
+      `Started refreshing ${commandList.length} application (/) commands.`
     );
-
-    // The put method is used to fully refresh all commands in the guild with the current set
+    // Routes.applicationCommands(CLIENT_ID) -> Global commands route
+    // Routes.applicationGuildCommands(CLIENT_ID, DEV_SERVER_ID) -> Server specific commands route
     const data = await rest.put(
       Routes.applicationGuildCommands(CLIENT_ID, DEV_SERVER_ID),
-      { body: commands }
+      {
+        body: commandList.map((command: Command) => {
+          return command.data.toJSON();
+        }),
+      }
     );
-    // const data = await rest.put(Routes.applicationCommands(CLIENT_ID), {
-    //   body: commands,
-    // });
     console.log(`Successfully reloaded ${data} application (/) commands.`);
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
     console.error(error);
   }
 })();
