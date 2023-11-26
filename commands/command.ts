9499e08596796cbd50aea96e0ded61885c86af4c
caelebk <khocaeleb@gmail.com>
2023-11-25 16:48:34 -0800

    updated command interface and list

--- commands/command.ts
+++ commands/command.ts
@@ -1,10 +1,11 @@
+import myClient from "..";
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { serverCommand } from "./info/server";
-import myClient from "..";
+import { partyCommand } from "./party/party";
 
 export interface Command {
   data: SlashCommandBuilder;
   execute: (client: myClient, interaction: CommandInteraction) => void;
 }
 
-export const commandList: Command[] = [serverCommand];
+export const commandList: Command[] = [serverCommand, partyCommand];
