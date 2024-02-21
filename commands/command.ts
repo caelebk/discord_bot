a3b168ae032a8a9d67dbbb2c663daa151383d692
Caeleb <khocaeleb@gmail.com>
2024-02-21 01:17:33 -0800

    MVP for opgg command

--- commands/command.ts
+++ commands/command.ts
@@ -2,10 +2,15 @@ import myClient from "..";
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { serverCommand } from "./info/server";
 import { partyCommand } from "./party/party";
+import { opggCommand } from "./lol/opgg";
 
 export interface Command {
   data: SlashCommandBuilder;
   execute: (client: myClient, interaction: CommandInteraction) => void;
 }
 
-export const commandList: Command[] = [serverCommand, partyCommand];
+export const commandList: Command[] = [
+  serverCommand,
+  partyCommand,
+  opggCommand,
+];
