a7bb2d9b70e713f7eea74826371290e39751a0fc
Caeleb <khocaeleb@gmail.com>
2024-02-21 16:43:56 -0800

    ported opgg

--- commands/lol/opgg.ts
+++ commands/lol/opgg.ts
@@ -7,6 +7,7 @@ import { Command } from "../command";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import myClient from "../..";
+import { isWhitespace } from "../../utility/string/stringUtility";
 
 export const opggCommand: Command = {
   data: new SlashCommandBuilder()
@@ -21,15 +22,17 @@ export const opggCommand: Command = {
   async execute(client: myClient, interaction: CommandInteraction) {
     const input = interaction.options.get("user");
     let username: string = input?.value as string;
-    username = username.replace("#", "-");
+    username = username.trim().replace("#", "-").replace(" ", "%20");
+    console.log(username);
     const baseUrl = "https://op.gg/summoners/na/";
     scrape(baseUrl + username)
       .then((value: EmbedBuilder[]) => {
         interaction.reply({ embeds: value });
       })
       .catch((reason: any) => {
-        // interaction.reply("User was not found.");
         console.log(reason);
+        const error = reason as Error;
+        interaction.reply(error.message);
       });
   },
 };
@@ -45,197 +48,157 @@ async function scrape(url: string): Promise<EmbedBuilder[]> {
     .remove()
     .end()
     .text();
-  let profileLadder = profile.find(".rank").text();
-  profileLadder = profileLadder.split("\n").slice(-2).join(" ").trim();
-  const profileImage = profile.find(".profile-icon").find("img").attr("src");
 
+  if (isWhitespace(profileName)) {
+    throw new Error(
+      "Profile not found or has a custom background that prevents me from parsing the profile."
+    );
+  }
+  let profileLadder = profile.find(".rank").text();
+  profileLadder =
+    profileLadder.length > 0
+      ? profileLadder.split("\n").slice(-2).join(" ").trim()
+      : "Player has yet to play ranked.";
+  const profileImage = profile.find(".profile-icon").find("img").attr("src");
+  const profileLevel = profile.find("span[class='level']").text();
   const profileStats = {
     name: profileName,
     ladder: profileLadder,
     imgSrc: profileImage,
+    level: profileLevel,
   } as Profile;
-
   console.log(profileStats);
-
-  const rankedSolo = $(".css-1kw4425");
-  let rankedSoloRank = rankedSolo.find(".tier").text();
-  rankedSoloRank =
-    rankedSoloRank.charAt(0).toUpperCase() + rankedSoloRank.slice(1);
-  const rankedSoloLP = rankedSolo.find(".lp").text();
-  const rankedSoloWinRate = rankedSolo.find(".ratio").text().slice(-3);
-  const rankedSoloWinLoss = rankedSolo.find(".win-lose").text();
-  const rankedSoloImage = rankedSolo.find("img").attr("src");
-
-  const rankedSoloStats = {
-    title: "Ranked Solo/Duo",
-    rank: rankedSoloRank,
-    lp: rankedSoloLP,
-    winRate: rankedSoloWinRate,
-    winLoss: rankedSoloWinLoss,
-    imgSrc: rankedSoloImage,
-  } as RankedStats;
-  console.log(rankedSoloStats);
-
-  const rankedFlex = $(".css-1ialdhq");
-  let rankedFlexRank = rankedFlex.find(".tier").text();
-  rankedFlexRank =
-    rankedFlexRank.charAt(0).toUpperCase() + rankedFlexRank.slice(1);
-  const rankedFlexLP = rankedFlex.find(".lp").text();
-  const rankedFlexWinRate = rankedFlex.find(".ratio").text().slice(-3);
-  const rankedFlexWinLoss = rankedFlex.find(".win-lose").text();
-  const rankedFlexImage = rankedFlex.find("img").attr("src");
-
-  const rankedFlexStats = {
-    title: "Ranked Flex",
-    rank: rankedFlexRank,
-    lp: rankedFlexLP,
-    winRate: rankedFlexWinRate,
-    winLoss: rankedFlexWinLoss,
-    imgSrc: rankedFlexImage,
-  } as RankedStats;
-  console.log(rankedFlexStats);
 
   const profileEmbed = new EmbedBuilder()
     .setColor(0x0099ff)
-
     .setTitle(`${profileStats.name}`)
-    .setURL(url)
-    .addFields({
-      name: "Ladder Rank",
-      value: profileStats.ladder,
-    })
+    .addFields(
+      {
+        name: "Level",
+        value: profileStats.level,
+        inline: true,
+      },
+      {
+        name: "Ladder Rank",
+        value: profileStats.ladder,
+        inline: true,
+      }
+    )
     .setThumbnail(
       "https://i0.wp.com/log.op.gg/wp-content/uploads/2022/01/cropped-opgg_favicon.png?fit=512%2C512&ssl=1"
     );
-  //https://s-lol-web.op.gg/images/reverse.rectangle.png
-  //https://i0.wp.com/log.op.gg/wp-content/uploads/2022/01/cropped-opgg_favicon.png?fit=512%2C512&ssl=1
 
-  const rankedSoloEmbed = new EmbedBuilder()
-    .setColor(0x0099ff)
-    .setAuthor({
-      name: profileStats.name,
-      iconURL: profileStats.imgSrc,
-      url: url,
-    })
-    .setTitle(rankedSoloStats.title)
-    .setThumbnail(rankedSoloStats.imgSrc)
-    .addFields(
-      {
-        name: rankedSoloStats.rank,
-        value: rankedSoloStats.lp,
-        inline: true,
-      },
-      {
-        name: "Win Rate",
-        value: rankedSoloStats.winRate,
-        inline: true,
-      },
-      {
-        name: "Win Loss",
-        value: rankedSoloStats.winLoss,
-        inline: true,
-      }
-    );
+  const embeds: EmbedBuilder[] = [profileEmbed];
 
-  const rankedFlexEmbed = new EmbedBuilder()
-    .setColor(0x0099ff)
-    .setAuthor({
-      name: profileStats.name,
-      iconURL: profileStats.imgSrc,
-      url: url,
-    })
-    .setTitle(rankedFlexStats.title)
-    .setThumbnail(rankedFlexStats.imgSrc)
-    .addFields(
-      {
-        name: rankedFlexStats.rank,
-        value: rankedFlexStats.lp,
-        inline: true,
-      },
-      {
-        name: "Win Rate",
-        value: rankedFlexStats.winRate,
-        inline: true,
-      },
-      {
-        name: "Win Loss",
-        value: rankedFlexStats.winLoss,
-        inline: true,
-      }
-    );
-
-  return [profileEmbed, rankedSoloEmbed, rankedFlexEmbed];
-}
-
-async function oldScraper(url: string) {
-  const { data } = await axios.get(url);
-  const $ = cheerio.load(data);
-  const summary = $('meta[name="description"]').attr("content");
-
-  const stats = summary?.split("/");
-
-  if (!stats) {
-    return "Failed to find stats.";
+  const rankedSoloClass = ".css-1kw4425";
+  const rankedSoloStats = parseRankedStats(
+    $,
+    rankedSoloClass,
+    "Ranked Solo/Duo"
+  );
+  console.log(rankedSoloStats);
+  if (rankedSoloStats) {
+    const rankedSoloEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setAuthor({
+        name: profileStats.name,
+        iconURL: profileStats.imgSrc,
+        url: url,
+      })
+      .setTitle(rankedSoloStats.title)
+      .setThumbnail(rankedSoloStats.imgSrc)
+      .addFields(
+        {
+          name: rankedSoloStats.rank,
+          value: rankedSoloStats.lp,
+          inline: true,
+        },
+        {
+          name: "Win Rate",
+          value: rankedSoloStats.winRate,
+          inline: true,
+        },
+        {
+          name: "Win Loss",
+          value: rankedSoloStats.winLoss,
+          inline: true,
+        }
+      );
+    embeds.push(rankedSoloEmbed);
   }
 
-  for (let x = 0; x < stats.length; x++) {
-    stats[x] = stats[x].trim();
+  const rankedFlexStats = parseRankedStats($, ".css-1ialdhq", "Ranked Flex");
+  console.log(rankedFlexStats);
+  if (rankedFlexStats) {
+    const rankedFlexEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setAuthor({
+        name: profileStats.name,
+        iconURL: profileStats.imgSrc,
+        url: url,
+      })
+      .setTitle(rankedFlexStats.title)
+      .setThumbnail(rankedFlexStats.imgSrc)
+      .addFields(
+        {
+          name: rankedFlexStats.rank,
+          value: rankedFlexStats.lp,
+          inline: true,
+        },
+        {
+          name: "Win Rate",
+          value: rankedFlexStats.winRate,
+          inline: true,
+        },
+        {
+          name: "Win Loss",
+          value: rankedFlexStats.winLoss,
+          inline: true,
+        }
+      );
+    embeds.push(rankedFlexEmbed);
   }
 
-  if (stats.length <= 1) {
-    return "User was not found.";
+  return embeds;
+}
+
+function parseRankedStats(
+  $: cheerio.CheerioAPI,
+  elementClass: string,
+  title: string
+): RankedStats | null {
+  const rankHtml = $(elementClass);
+  let rank = rankHtml.find(".tier").text();
+  rank = rank.charAt(0).toUpperCase() + rank.slice(1);
+  const lp = rankHtml.find(".lp").text();
+  const winRate = rankHtml.find(".ratio").text().slice(-3);
+  const winLoss = rankHtml.find(".win-lose").text();
+  const imageSrc = rankHtml.find("img").attr("src");
+
+  if (
+    isWhitespace(rank) ||
+    isWhitespace(lp) ||
+    isWhitespace(winRate) ||
+    isWhitespace(winLoss) ||
+    imageSrc === null
+  ) {
+    return null;
   }
-
-  return createStatisticsString(stats);
+  return {
+    title: title,
+    rank: rank,
+    lp: lp,
+    winRate: winRate,
+    winLoss: winLoss,
+    imgSrc: imageSrc,
+  } as RankedStats;
 }
 
-function createStatisticsString(stats: string[]) {
-  if (stats.length < 4) {
-    return (
-      "**Name: **" + stats[0] + "\n**Level: **" + stats[2] + "\nNo Stats Found."
-    );
-  }
-
-  let msg = createNameString(stats[0]);
-  msg += createRankString(stats[1]);
-  msg += createWinRateString(stats[2]);
-  msg += createChampsString(stats[3]);
-  return msg;
-}
-
-function createNameString(name: string) {
-  return `**Name:** ${name}\n`;
-}
-
-function createRankString(rank: string) {
-  const rankinfo = rank.split(" ");
-  if (rankinfo.length < 3) {
-    return `**Rank:** ${rankinfo[0]} ${rankinfo[1]}\n`;
-  } else {
-    return `**Rank:** ${rankinfo[0]} ${rankinfo[1]} | ${rankinfo[2]}\n`;
-  }
-}
-
-function createWinRateString(winRate: string) {
-  const ratio = winRate.split(" ");
-  return `**Win Rate:** ${ratio[4]} |  ${ratio[0].replace(
-    "Win",
-    "W"
-  )}  -  ${ratio[1].replace("Lose", "L")}\n`;
-}
-
-function createChampsString(champs: string) {
-  const champinfo = champs.split(",");
-  let msg = "**Most Played Champions:**\n**--------------------------**";
-  for (var x = 0; x < champinfo.length; x++) {
-    msg += "\n" + champinfo[x].trim().replace("Win", "W").replace("Lose", "L");
-  }
-  return msg + "\n";
-}
 interface Profile {
   name: string;
   ladder: string;
   imgSrc: string;
+  level: string;
 }
 
 interface RankedStats {
---
+++ utility/string/stringUtility.ts
@@ -0,0 +1,3 @@
+export function isWhitespace(s: string): boolean {
+  return s.trim() === "";
+}
